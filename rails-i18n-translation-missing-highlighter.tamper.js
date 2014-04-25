f4d5adfb00752fca54b5285cd8d86e0e9a872aa7
Tobias Jordans <t@tobiasjordans.de>
2014-04-25 10:09:29 +0200

    First version of the script

---
+++ rails-i18n-translation-missing-highlighter.tamper.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name       rails-i18n-translation-missing-highlighter
+// @namespace  http://www.betterplace.org/
+// @version    0.1
+// @description  See https://github.com/tordans/rails-i18n-translation-missing-highlighter
+// @match      http://www.betterplace.dev/*, https://www.bp42.com/*, https://www.betterplace.org/* 
+// @copyright  2012+, Tobias Jordans
+// ==/UserScript==
+
+var $translation_missing_elements = $("[title*='translation missing:']");
+var $quickinfo_element = $('.quickinfo.generic-admin-box.for-platform-admins');
+
+if($translation_missing_elements.length > 0) {
+	$translation_missing_elements.each( function() {
+		$quickinfo_element.append("<h4>" + $(this).attr('title') + "</h4>"); 
+	});
+} else {
+    $quickinfo_element.append(" / no translations missing"); 
+}
